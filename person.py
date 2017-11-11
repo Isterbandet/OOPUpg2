b4701ee64e4f27523e8ac9ef165380a20e8fd240
Anton <anton_eklof@hotmail.com>
2017-11-11 11:10:02 +0100

    Kort fortsättning

---
+++ person.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+
+
+
+
+    def getName(self):
+        return self.name
+    def getAddress(self):
+        return self.address
+
+    def setAddress(self,address):
+        self.address = address
